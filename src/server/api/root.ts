0ceaff5499c0e682489287bdc0c4fef5f0038c20
esponges <esponges@gmail.com>
2023-04-09 15:56:25 -0600

    add request to openAI

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { drinksRouter } from "./routers/drinks";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  drinks: drinksRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/drinks.ts
@@ -0,0 +1,50 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+import { env } from "~/env.mjs";
+
+import type { CreateCompletionRequest} from "openai";
+import { Configuration, OpenAIApi } from "openai";
+
+const openAIConfig = new Configuration({
+  organization: env.OPENAI_ORG_ID,
+  apiKey: env.OPENAI_API_KEY,
+});
+
+const openAI = new OpenAIApi(openAIConfig);
+
+export const drinksRouter = createTRPCRouter({
+  create: publicProcedure
+    .input(z.object({ brand: z.string() }))
+    .mutation(async ({ input }) => {
+      // TODO: make the call to the OpenAI API
+      const requestObject: CreateCompletionRequest = {
+        prompt: `I want a ${input.brand} cocktail recipe`,
+        // model gpt 3.5
+        model: "gpt-3.5-turbo",
+        max_tokens: 100,
+        temperature: 0.9,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      };
+
+      try {
+        const recipe = await openAI.createCompletion(requestObject);
+        console.log('recipe', recipe.data?.choices[0]?.text);
+        return {
+          recipe: recipe.data?.choices[0]?.text,
+        };
+      } catch (error) {
+        console.log(error);
+        return {
+          recipe: "Sorry, I don't know that brand.",
+        };
+      }
+    }),
+});
