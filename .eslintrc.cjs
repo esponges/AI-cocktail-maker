88c1e54ebf1754df93b1fa93c57b6c18b4f1a1cd
esponges <esponges@gmail.com>
2023-04-09 15:55:55 -0600

    import rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,14 +21,44 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
+    "@typescript-eslint/consistent-type-imports": "warn",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/ban-types": [
+      "error",
       {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
+        extendDefaults: true,
+        types: {
+          "{}": false,
+        },
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    // newline after import - requires import-newlines plugin
+    "max-len": ["error", 120],
+    // "import-newlines/enforce": ["error", 2, 100],
+    // newline on React component props
+    "react/jsx-closing-bracket-location": [
+      "error",
+      {
+        selfClosing: "line-aligned",
+        nonEmpty: "line-aligned",
+      },
+    ],
+    indent: "off",
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/semi": ["error", "always"],
+    "@typescript-eslint/no-shadow": "error",
+    // no template curly braces in strings and no unnecessary template literals
+    "no-template-curly-in-string": "error",
+    // don't allow needless curly braces around static values
+    "react/jsx-curly-brace-presence": [
+      "warn",
+      { props: "never", children: "never" },
+    ],
+    // jsx curly spacing
+    "react/jsx-curly-spacing": ["error", "never"],
   },
 };
 
